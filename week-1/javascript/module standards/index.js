da32228a2b9b7ed63cbccfe51da9b123be217dd3
Nagabhushanam <nagabhushanamn@gmail.com>
2021-02-04 00:02:31 +0530

    ESM

--- week-1/javascript/module standards/index.js
+++ week-1/javascript/module standards/index.js
@@ -1,9 +1,26 @@
 
 
-// 
+// const greet = require('batch2-greeting')
+// greet('en')
+// greet('es')
+// greet('ar')
 
-const greet = require('batch2-greeting')
 
-greet('en')
-greet('es')
-greet('ar')
+// import primaryItem, { item1, item2 } from './menu.js';
+// console.log(primaryItem);
+// console.log(item1);
+// console.log(item2);
+
+
+// import * as menu from './menu.js'
+// console.log(menu.default);
+// console.log(menu.item1);
+// console.log(menu.item2);
+
+
+// import fav, { item1 as fav1, item2 as fav2 } from './menu.js'
+// console.log(fav);
+// console.log(fav1);
+// console.log(fav2);
+
+
