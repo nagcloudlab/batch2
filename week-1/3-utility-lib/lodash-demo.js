f146bb036f587fedbb93a8634069aac51cd3f5e6
Nagabhushanam <nagabhushanamn@gmail.com>
2021-02-06 02:06:18 +0530

    week 1 commit

---
+++ week-1/3-utility-lib/lodash-demo.js
@@ -0,0 +1,10 @@
+const _ = require('lodash')
+
+const arr1 = [1, 2, 3, 4]
+const arr2 = [1, 3, 4]
+
+
+const diff = _.difference(arr1, arr2)
+console.log(diff)
+
+//----------------------------------------------------------------
---
+++ week-1/3-utility-lib/moment-demo.js
@@ -0,0 +1,6 @@
+
+var moment = require('moment');
+
+
+var now = moment();//..
+console.log(now.format("dddd, MMMM Do YYYY, h:mm:ss a"));
