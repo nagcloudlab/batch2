d9f67365d761e77c4746d185fd2761db80163535
Nagabhushanam <nagabhushanamn@gmail.com>
2021-02-10 02:57:32 +0530

    css intro

--- week-2/webui/src/index.js
+++ week-2/webui/src/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css'
 
 //----------------------------------------------------------------
 // DOM API
